fa9bde733a4f866aa3f3f912016cc9ce4eeca994
doanga2007 <wig_2006@hotmail.com>
2019-09-25 12:15:24 +0700

    Update.

---
+++ GenericRepeater/Default.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="GenericRepeater.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+
+    <asp:Repeater ID="rptName" runat="server">
+    <HeaderTemplate>
+    <table>
+         <tr>
+                <th>EmployeeID</th>
+                <th>DepartmentID</th>
+                <th>EmployeeName</th>
+        </tr>   
+    </HeaderTemplate>
+      <ItemTemplate>
+        <tr>
+          <td>
+            <%# ((Employee)Container.DataItem).EmpID %>
+          </td>
+          <td>
+            <%# ((Employee)Container.DataItem).DeptID %>
+          </td>
+          <td>
+            <%# ((Employee)Container.DataItem).EmpName %>
+          </td>
+        </tr>
+      </ItemTemplate>
+      <FooterTemplate>      
+        </table><br />
+    </FooterTemplate>
+    </asp:Repeater>
+
+    </form>
+</body>
+</html>
---
+++ GenericRepeater/Default.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GenericRepeater
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            rptName.DataSource = EmployeeList.emp;
+            rptName.DataBind();
+        }
+        public class EmployeeList
+        {
+            static EmployeeList()
+            {
+                emp = new List<Employee>();
+                emp.Add(new Employee() { EmpID = 1, DeptID = 1, EmpName = "Jack Nolas" });
+                emp.Add(new Employee() { EmpID = 2, DeptID = 4, EmpName = "Mark Pine" });
+                emp.Add(new Employee() { EmpID = 3, DeptID = 3, EmpName = "Sandra Simte" });
+                emp.Add(new Employee() { EmpID = 4, DeptID = 4, EmpName = "Larry Lo" });
+                emp.Add(new Employee() { EmpID = 5, DeptID = 3, EmpName = "Sudhir Panj" });
+                emp.Add(new Employee() { EmpID = 6, DeptID = 2, EmpName = "Kathy K" });
+                emp.Add(new Employee() { EmpID = 7, DeptID = 1, EmpName = "Kaff Joe" });
+                emp.Add(new Employee() { EmpID = 8, DeptID = 1, EmpName = "Su Lie" });
+            }
+
+            public static List<Employee> emp { get; set; }
+        }
+
+        public class Employee
+        {
+            public int EmpID { get; set; }
+            public int DeptID { get; set; }
+            public string EmpName { get; set; }
+        }
+    }
+}
